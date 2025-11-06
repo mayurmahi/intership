57e3a4dc6c7a0dd2d6bc728a3da87a8f12b298d1
mayurmahi <mayurmahindrakar444@gmail.com>
2025-11-06 15:21:41 +0530

    vercel

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import FeatureCards from './components/FeatureCards';
